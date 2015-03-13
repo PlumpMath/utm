dadc6f31e9dd6eb318ccfadd38c7ca1f1ac15705
Mark Farrell <m4farrel@uwaterloo.ca>
2015-03-13 09:34:37 -0300

    Project WGS84 coordinates.
    Signed-off-by: Mark Farrell <m4farrel@uwaterloo.ca>

---
+++ test/utm-tests.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(provide utm-zone-tests
+         utm-tests)
+
+(require "../data/utm.rkt")
+(require rackunit)
+
+;; (: utm-zone-tests TestSuite)
+(define utm-zone-tests
+  (test-suite
+    "UTM Zone Tests"
+    (check-equal? (utm-zone -55.371094 46.794044) 21)
+    (check-equal? (utm-zone -18.105469 65.105450) 27)
+    (check-equal? (utm-zone 110.390625 64.616345) 49)
+    (check-equal? (utm-zone 28.476563  64.990511) 35)))
+
+;; (: utm-tests TestSuite)
+(define utm-tests
+  (test-suite
+    "UTM Tests"
+    (check-equal? (utm 2.134 50.340)
+                  (vector 438377.19126793207 5577131.769912637))))
